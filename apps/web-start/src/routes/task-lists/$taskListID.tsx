e1bfb3165193924d0057b5823e212cdb6e2dbd46
TheLuckyman30 <bnauta0@gmail.com>
2025-11-03 04:32:56 -0500

    Tasks are now retrived from database

--- apps/web-start/src/routes/task-lists/$taskListID.tsx
+++ apps/web-start/src/routes/task-lists/$taskListID.tsx
@@ -1,5 +1,9 @@
 import { Link, createFileRoute } from '@tanstack/react-router';
 import './../../components/button.css';
+import { useQuery } from '@tanstack/react-query';
+import { backendFetcher } from '../../integrations/fetcher';
+import type { TaskListOut } from '@repo/api/task-list';
+import type { TaskOut } from '@repo/api/task';
 
 export const Route = createFileRoute('/task-lists/$taskListID')({
   component: TaskList,
@@ -7,41 +11,58 @@ export const Route = createFileRoute('/task-lists/$taskListID')({
 
 function TaskList() {
   const { taskListID } = Route.useParams();
-
-  return (
-    <body style={{ backgroundColor: '#815656' }}>
-      <div>
-        <h1
-          style={{
-            fontSize: '20px',
-            color: 'white',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: '20vh',
-          }}
-        >
-          Task List: {taskListID}
-        </h1>
-        <br></br>
-
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            gap: '15vh', // space between buttons
-            marginTop: '10vh',
-          }}
-        >
-          <Link to="/task-lists" className="buttonStyling">
-            Task Lists
-          </Link>
-          <Link to="/create-task" className="buttonStyling">
-            Create a Task
-          </Link>
-        </div>
-      </div>
-    </body>
+  const { data: taskList, isFetching: listIsFetching } = useQuery<TaskListOut>({
+    queryKey: ['task-list', taskListID],
+    queryFn: backendFetcher<TaskListOut>(`/task-lists/${taskListID}`),
+  });
+  const { data: tasks, isFetching: tasksIsFetching } = useQuery<Array<TaskOut>>(
+    {
+      queryKey: ['tasks', taskListID],
+      queryFn: backendFetcher<Array<TaskOut>>(
+        `/task-lists/${taskListID}/tasks`,
+      ),
+      initialData: [],
+    },
   );
+
+  if (listIsFetching || tasksIsFetching) {
+    return <body style={{ backgroundColor: '#815656' }}>Loading...</body>;
+  }
+
+  if (taskList) {
+    return (
+      <body style={{ backgroundColor: '#815656' }}>
+        <div className="flex flex-col gap-20 justify-center items-center">
+          <h1
+            style={{
+              fontSize: '20px',
+              color: 'white',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: '20vh',
+            }}
+          >
+            Task List: {taskList.name}
+          </h1>
+          <br></br>
+          <div>
+            <Link to="/create-task" className="buttonStyling">
+              Create a Task
+            </Link>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="rounded-md bg-[#fddbcdeb] text-[#6c3b27ee] text-4xl p-1">
+              Tasks:
+            </div>
+            {tasks.map((task) => (
+              <div className="rounded-md bg-[#fddbcdeb] text-[#6c3b27ee] text-2xl p-1">
+                {task.name}
+              </div>
+            ))}
+          </div>
+        </div>
+      </body>
+    );
+  }
 }
