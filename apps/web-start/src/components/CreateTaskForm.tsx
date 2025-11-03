48659ae9542d310b7b883118d770fcd8b19da027
Ben Wootten <bwootten@udel.edu>
2025-11-03 00:44:36 -0500

    added frontend only submission functionality

--- apps/web-start/src/components/CreateTaskForm.tsx
+++ apps/web-start/src/components/CreateTaskForm.tsx
@@ -3,14 +3,29 @@ import './CreateTaskForm.css';
 import './button.css';
 
 export function CreateTaskForm(): React.JSX.Element {
+    const [taskName, setTaskName] = useState<string>("");
+    const [taskDescription, setTaskDescription] = useState<string>("");
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault(); // prevent page reload
+        console.log("Created Task: " + taskName + "\n" + taskDescription);
+    }
 
     return (
-        <div>
-            <p>Task Name</p>
-            <input type="text"></input>
-            <p>Task Description</p>
-            <input type="text"></input>
-            <button type="button" className="button">Submit</button>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <label>Task Name</label>
+            <input
+                type="text"
+                value={taskName}
+                onChange={(e) => setTaskName(e.target.value)}>
+            </input>
+            <label>Task Description</label>
+            <input
+                type="text"
+                value={taskDescription}
+                onChange={(e) => setTaskDescription(e.target.value)}>
+            </input>
+            <button type="submit" className="button">Submit</button>
+        </form>
     )
 }
