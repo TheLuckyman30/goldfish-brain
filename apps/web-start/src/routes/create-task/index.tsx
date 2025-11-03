9abcad0fcdd9f50c43e1fd38f8e577e5cd5b34d4
Ben Wootten <bwootten@udel.edu>
2025-11-02 22:51:37 -0500

    added create task page

---
+++ apps/web-start/src/routes/create-task/index.tsx
@@ -0,0 +1,17 @@
+import { Link, createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/create-task/')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <div>
+    <h1>Create task page</h1>
+    <Link
+        to='/task-lists/$taskListID'
+        params = {{taskListID: "groceries"/* placeholder*/ }}
+        className='button'
+    >Back to task list</Link>
+    <Link to="/" className="button">Home</Link>
+  </div>
+}
