22c925e3061636501195cebf705a51403d1e01a1
Selin A Bacaz <selinb@udel.edu>
2025-11-03 02:54:27 -0500

    added barebones stylization on tasks-lists page

--- apps/web-start/src/routes/task-lists/index.tsx
+++ apps/web-start/src/routes/task-lists/index.tsx
@@ -1,18 +1,36 @@
 import { Link, createFileRoute } from '@tanstack/react-router'
 import './../../components/button.css';
+import { Navbar } from '../../components/navbar';
+import { Sidebar } from '../../components/sidebar';
 
 export const Route = createFileRoute('/task-lists/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>
-    <h1>Task Lists Page</h1>
-    <Link
-        to='/task-lists/$taskListID'
-        params = {{taskListID: "groceries"/* placeholder*/ }}
-        className='button'
-    >View a task List</Link>
-    <Link to="/" className="button">Home</Link>
-  </div>
+  return(
+    
+     <body style={{backgroundColor: "#815656"}}>
+      
+      <Sidebar isOpen={false} setIsOpening={function (isOpen: boolean): void {
+          throw new Error('Function not implemented.');
+        } }></Sidebar>
+      <Navbar></Navbar>
+      <div style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "15vh", // space between buttons
+          marginTop: "40vh",
+        }}>
+        <Link
+            to='/task-lists/$taskListID'
+            params = {{taskListID: "groceries"/* placeholder*/ }}
+            className='buttonStyling'
+            
+        >View a task List</Link>
+        <Link to="/" className="buttonStyling">Home</Link>
+      </div>
+  </body>
+  );
 }
