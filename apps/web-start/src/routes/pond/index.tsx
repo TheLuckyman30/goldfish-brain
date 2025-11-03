69d17c458f0a91aa53e5d1b02fc44c077464acae
Ben Wootten <bwootten@udel.edu>
2025-11-02 22:52:08 -0500

    added routes to pond page

--- apps/web-start/src/routes/pond/index.tsx
+++ apps/web-start/src/routes/pond/index.tsx
@@ -1,4 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { Link, createFileRoute } from '@tanstack/react-router';
+import './../styles.css'
 
 export const Route = createFileRoute('/pond/')({
   component: RouteComponent,
@@ -7,5 +8,11 @@ export const Route = createFileRoute('/pond/')({
 function RouteComponent() {
   return <div>
     <h1>Pond Page</h1>
+    <Link to='/' className='button'>Home</Link>
+    <Link
+      to='/task-lists/$taskListID'
+      params = {{taskListID: "groceries"/* placeholder*/ }}
+      className='button'
+    >View a task List</Link>
   </div>
 }
