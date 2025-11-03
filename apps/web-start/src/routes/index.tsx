2a4a5cf019912709253dbad5f6e389a6bf077d16
Ben Wootten <bwootten@udel.edu>
2025-11-02 21:54:31 -0500

    added link to pond

--- apps/web-start/src/routes/index.tsx
+++ apps/web-start/src/routes/index.tsx
@@ -1,9 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { Link, createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>;
+  return <div>
+    <h1>Landing Page</h1>
+    <Link to='/pond' className="button">pond</Link>
+  </div>
 }
