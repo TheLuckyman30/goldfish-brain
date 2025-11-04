7db41330a2b2fc31fedd429a4b5c303da8bba958
TheLuckyman30 <bnauta0@gmail.com>
2025-11-04 01:20:49 -0500

    Added guards to endpoints and added auth module

--- apps/api/src/folders/folders.controller.ts
+++ apps/api/src/folders/folders.controller.ts
@@ -1,16 +1,19 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { FoldersService } from './folders.service';
 import { FolderOut } from '@repo/api/folder';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('folders')
 export class FoldersController {
   constructor(private foldersService: FoldersService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   findAll(): Promise<FolderOut[]> {
     return this.foldersService.findAllFolders({});
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   find(@Param('id') folderId: string): Promise<FolderOut> {
     return this.foldersService.findFolder({ id: folderId });
--- apps/api/src/tasks/tasks.controllers.ts
+++ apps/api/src/tasks/tasks.controllers.ts
@@ -1,21 +1,25 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTask, TaskOut } from '@repo/api/task';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   findAll(): Promise<TaskOut[]> {
     return this.tasksService.findAllTasks({});
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   find(@Param('id') taskId: string): Promise<TaskOut> {
     return this.tasksService.findTask({ id: taskId });
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() newTask: CreateTask): Promise<TaskOut> {
     return this.tasksService.createTask(newTask);
