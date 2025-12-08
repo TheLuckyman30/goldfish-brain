97958d41b98c1815c56f5e058b7fce8cb0b232da
Ben Wootten <bwootten@udel.edu>
2025-12-07 20:54:37 -0500

    revamped settings page to profile

--- apps/web-start/src/routes/_protected-routes/settings/index.tsx
+++ apps/web-start/src/routes/_protected-routes/settings/index.tsx
@@ -1,26 +1,37 @@
 import { Link, createFileRoute } from '@tanstack/react-router';
 import '../../../components/button.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import fishAnimate from '../../../images/fishAnimate.gif';
 import { Loading } from '../../../components/loading/loadingScreen';
 import Button from '../../../components/shared-ui/Button';
 import { useApiMutation, useApiQuery } from '../../../integrations/api';
 import { EditUsernameForm } from '../../../components/settings/EditUsernameForm'
+import userImage from '../../../images/user.png';
+import type { UserOut } from '@repo/api/user';
 
 export const Route = createFileRoute('/_protected-routes/settings/')({
   component: Settings,
 });
 
-function toggleNight() {
-
-}
-
 function Settings() {
   const [showForm, setShowForm] = useState<boolean>(false);
   const [PasswordText, setPasswordText] = useState<string>("");
-  const {data, isFetching} = useApiQuery<{provider: string}>(['auth-type'], '/users/me/auth-type');
-  const provider = data?.provider ?? 'external';
+  // provider data
+  const {data: providerData, isFetching: isFetchingProvider} = useApiQuery<{provider: string}>(['auth-type'], '/users/me/auth-type');
+  const provider = providerData?.provider ?? 'external';
   const isExternalAccount = provider !== 'auth0';
+  // user data
+  const {data: userData, isFetching: isFetchingUser} = useApiQuery<UserOut>(['me'], '/users/me')
+  const [username, setUsername] = useState<string>("");
+  useEffect(() => {
+    if (userData) {
+      setUsername(userData.username);
+    } else {
+      setUsername("Failed to fetch username.")
+    }
+  }, [userData]);
+  const email = userData?.email;
+  // reset password
   const resetPasswordMutation = useApiMutation({
     endpoint: () => ({
       path: '/users/reset-password',
@@ -29,7 +40,7 @@ function Settings() {
   });
 
   function updateUsername() {
-   setShowForm(true); 
+   setShowForm(true);
   }
 
   function resetPassword() {
@@ -41,20 +52,20 @@ function Settings() {
     }
   }
 
-  if (isFetching) {
+  if (isFetchingProvider || isFetchingUser) {
     return <Loading></Loading>;
   }
 
   return (
     <div
-      className="flex justify-center min-h-screen  w-lvw pt-45 bg-no-repeat bg-cover bg-top"
+      className="flex justify-center min-h-screen  w-lvw pt-45 bg-no-repeat bg-cover bg-top pb-[20px]"
       style={{
         backgroundImage: `url(${fishAnimate})`,
       }}
     >
-      <div className="flex flex-col w-[75%] bg-[#538f97] rounded-lg shadow-[5px_5px_0px_0px_rgba(0,0,0,0.5)]">
+      <div className="flex flex-col w-[75%] bg-[#538f97] rounded-lg shadow-[5px_5px_0px_0px_rgba(0,0,0,0.5)] pb-[20px]">
         <h1 className="text-5xl text-center rounded-md bg-[#fddbcd] p-10 text-[#794531fb]">
-          Settings Page
+          Profile
         </h1>
 
         <Link
@@ -63,22 +74,49 @@ function Settings() {
         >
           Home
         </Link>
-        <div className="text-2xl text-center items-center ml-35 rounded-md bg-[#fddbcd] p-10 text-[#794531fb] max-w-5xl justify-center">
-          Edit Account Details
-          <br></br>{isExternalAccount ? "You may not edit details from an external account, please update those details from your " + provider + " account." : ""}
-          <Button onClick={() => updateUsername()} disabled={isExternalAccount}>Update Username</Button>
-          <Button onClick={() => resetPassword()} disabled={isExternalAccount}>Send Password Reset Email</Button>
-          <p>{PasswordText}</p>
+        <div className="px-4 sm:px-6 lg:px-12">
+          <div className="w-full max-w-5xl mx-auto rounded-md bg-[#fddbcd] p-10 text-[#794531fb]">
+            <div className="grid grid-cols-[auto_auto_minmax(0,1fr)] gap-4 text-2xl items-center">
+              <div>
+                <img src={userImage} className="max-w-[100px]"></img>
+              </div>
+              <div className="text-left">
+                <p className="p-4">Username:</p>
+                <p className="p-4">Email:</p>
+              </div>
+              <div className="text-left break-words min-w-0">
+                <p className="p-4">{username}</p>
+                <p className="p-4">{email}</p>
+              </div>
+            </div>
+          </div>
         </div>
         <br></br>
-        <div className="text-2xl text-center items-center ml-35 rounded-md bg-[#fddbcd] p-10 text-[#794531fb] max-w-5xl justify-center">
-          Themes
-          <Button onClick={() => toggleNight()}>Toggle Night Mode</Button>
+        <div className="px-4 sm:px-6 lg:px-12">
+          <div className="rounded-md bg-[#fddbcd] p-10 text-[#794531fb] text-2xl text-center items-center">
+            Edit Account Details
+            <br></br>{isExternalAccount ? "You may not edit details from an external account, please update those details from your " + provider + " account." : ""}
+            <Button
+              style={{
+                color: 'white',
+                background: '#6c3b27d7',
+              }}
+              onClick={() => updateUsername()} disabled={isExternalAccount}
+            >Update Username</Button>
+            <Button 
+              style={{
+                color: 'white',
+                background: '#6c3b27d7',
+              }}
+              onClick={() => resetPassword()} disabled={isExternalAccount}>Send Password Reset Email</Button>
+            <p>{PasswordText}</p>
+          </div>
         </div>
       </div>
       <EditUsernameForm
         showForm={showForm}
         setShowForm={setShowForm}
+        setUsername={setUsername}
       />
     </div>
   );
